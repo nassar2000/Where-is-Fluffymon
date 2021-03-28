66b6373ec894744ae6d1f35ccf553113bd42cc02
nassar2000 <nassarm65@gmail.com>
2021-03-28 12:05:09 +0300

    bbk for user service

---
+++ UserService/UserService/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using UserService.Entities;
+using UserService.Models.Users;
+
+namespace UserService.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<RegisterModel, User>();
+            CreateMap<UpdateModel, User>();
+        }
+    }
+}
---
+++ UserService/UserService/Models/Users/UserModel.cs
@@ -0,0 +1,10 @@
+namespace UserService.Models.Users
+{
+  public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
