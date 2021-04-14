4d0927ad3b79c6fbbcab9e8686e042376361f861
RaduTarsa <36694856+RaduTarsa@users.noreply.github.com>
2021-04-14 17:52:57 +0300

    Project Renewed
    We moved from API + UI to MVC

---
+++ Where-is-Fluffymoon/Where-is-Fluffymoon/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Where_is_Fluffymoon.Models;
+
+namespace Where_is_Fluffymoon.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Where_is_Fluffymoon.Models.Pet> Pet { get; set; }
+    }
+}
---
+++ Where-is-Fluffymoon/Where-is-Fluffymoon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Where_is_Fluffymoon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
