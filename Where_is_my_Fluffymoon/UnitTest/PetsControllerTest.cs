bf587d9306d946b8b0d7963caabb86e56d1a96f8
nassar2000 <nassarm65@gmail.com>
2021-04-19 20:43:25 +0300

    start with unit test

---
+++ Where_is_my_Fluffymoon/UnitTest/PetsControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Where_is_my_Fluffymoon.Models;
+
+namespace UnitTest
+{
+    public class PetsControllerTest
+    {
+        List<Pet> petList;
+
+        public PetsControllerTest()
+        {
+
+        }
+
+        public void Create(Pet pet)
+        {
+            petList.Add(pet);
+
+        }
+
+        public List<Pet> GetPets()
+        {
+            return petList;
+        }
+
+    }
+}
