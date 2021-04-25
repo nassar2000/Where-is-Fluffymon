709b7d358df2bd070e70eaf56858166ea076f338
Catalin Crainiceanu <c.crainiceanu@gmail.com>
2021-04-25 22:56:16 +0300

    Changed UI

--- Where_is_my_Fluffymoon/Where_is_my_Fluffymoon/Views/Pets/Details.cshtml.cs
+++ Where_is_my_Fluffymoon/Where_is_my_Fluffymoon/Views/Pets/Details.cshtml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Device.Location;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Where_is_my_Fluffymoon.Views.Pets
 {
